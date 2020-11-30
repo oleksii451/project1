5365bc11a7ca72d7129592b0052ce11b23388c0d
oleksii <oleksij.yaroshenko@gmail.com>
2020-11-30 22:01:39 +0200

    completed destructuring

--- project1/script222.js
+++ project1/script222.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+function first( {
+    setTimeout(function)
+})
